25b136daabbd93dc608d649ff76496c1c4aa4218
DamyVegaLeiva <damyvegaleiva@gmail.com>
2023-06-24 00:19:50 -0500

    Providers img with border + fixed responsive scroll.

--- src/components/ProvidersContainer/ProvidersContainer.jsx
+++ src/components/ProvidersContainer/ProvidersContainer.jsx
@@ -5,17 +5,17 @@ const ProvidersContainer = ({ providers }) => {
   const providersResults = getProviders(providers);
 
   return (
-    <div className="mt-10 mb-5 ">
+    <div className="w-3/4 mx-auto mt-10 mb-5">
       <h2 className="mb-2 tracking-wide underline decoration lg:text-lg ">
         Available on:
       </h2>
       {providersResults?.length > 0 ? (
-        <div className="flex flex-row items-center justify-center">
+        <div className="flex flex-row flex-wrap items-center justify-center gap-3">
           {providersResults?.map((provider) => (
             <img
               key={provider.provider_id}
               src={IMG_URL + provider.logo_path}
-              className="w-10 mx-2 "
+              className="w-10 mx-2 rounded-xl"
             />
           ))}
         </div>
