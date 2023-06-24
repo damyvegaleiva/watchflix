126a80849d2f5f31551087cf384f8e7c925573c6
DamyVegaLeiva <damyvegaleiva@gmail.com>
2023-06-23 23:32:30 -0500

    Spinner centered in responsive.

--- src/components/SpinnerLoader/SpinnerLoader.jsx
+++ src/components/SpinnerLoader/SpinnerLoader.jsx
@@ -2,7 +2,7 @@ import "./SpinnerLoader.css";
 
 const SpinnerLoader = () => {
   return (
-    <div className="lds-ring">
+    <div className="flex justify-center lds-ring">
       <div></div>
       <div></div>
       <div></div>
