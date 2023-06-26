803c92773cf35add66223932604479f911e2cd6e
DamyVegaLeiva <damyvegaleiva@gmail.com>
2023-06-26 00:06:37 -0500

    Fixed bug with empty related movies/tv shows + fixed poster size responsive.

--- src/components/RecommendationsContainer/RecommendationsContainer.jsx
+++ src/components/RecommendationsContainer/RecommendationsContainer.jsx
@@ -1,21 +1,22 @@
 import { getModifiedData } from "../../utils/APIFunctions";
-import TrendingList from "../SliderList/SliderList";
+import SliderList from "../SliderList/SliderList";
 
 const RecommendationsContainer = ({ recommendations }) => {
   const related = getModifiedData(recommendations);
 
-  return (
-    <div className="w-3/5 m-auto">
-      <h2 className="w-full py-1 pl-4 mb-5 text-sm italic tracking-widest text-left text-white rounded-sm lg:text-xl bg-gradient-to-r from-red-600">
-        YOU MAY ALSO LIKE
-      </h2>
-      <TrendingList
-        data={related}
-        tvMovieValue={related[0].media_type}
-        difStyle={"lg:h-48 h-28 md:h-32"}
-      />
-    </div>
-  );
+  if (related.length > 0)
+    return (
+      <div className="w-3/4 m-auto md:w-3/5">
+        <h2 className="w-full py-1 pl-4 mb-5 text-sm italic tracking-widest text-left text-white rounded-sm lg:text-xl bg-gradient-to-r from-red-600">
+          YOU MAY ALSO LIKE
+        </h2>
+        <SliderList
+          data={related}
+          tvMovieValue={related[0].media_type}
+          difStyle={"h-24 sm:h-44 md:h-52 lg:h-56 xl:h-72"}
+        />
+      </div>
+    );
 };
 
 export default RecommendationsContainer;
