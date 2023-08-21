25a026b1cd2c9ff179da6f053c690f8fb5cb1a6a
DamyVegaLeiva <damyvegaleiva@gmail.com>
2023-08-21 14:14:00 -0500

    Minor fix.

--- src/containers/LoginRegisterContainer/LoginRegisterContainer.jsx
+++ src/containers/LoginRegisterContainer/LoginRegisterContainer.jsx
@@ -4,7 +4,7 @@ const LoginRegisterContainer = ({ FormContainer }) => {
       <img
         src="./images/login-bg.jpg"
         alt=""
-        className="hidden md:block md:object-cover md:w-screen md:h-screen md:absolute -z-10"
+        className="hidden opacity-60 md:block md:object-cover md:w-screen md:h-screen md:absolute -z-10"
       />
       <section className=" lg:w-[800px] md:h-[800px] rounded-md md:border py-5 flex gap-5 items-center justify-center flex-col backdrop-blur-sm bg-black/60">
         <img src="/images/watchflix.png" alt="Logo" className="" />
