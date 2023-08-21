5575d968c950db1494b0abb68410e82dae2a4f20
DamyVegaLeiva <damyvegaleiva@gmail.com>
2023-08-21 14:09:45 -0500

    Redesign of mobile version, changed button bg-color, minor fixes.

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({ title, handleSubmit }) => {
   return (
     <button
-      className="py-2 text-xs text-white transition duration-300 ease-in-out border w-36 lg:text-md lg:w-2/3 hover:border-red-600 hover:text-red-600 rounded-xl hover:bg-white"
+      className="py-2 text-xs text-white transition duration-300 ease-in-out border w-36 lg:text-md lg:w-2/3 hover:border-red-600 rounded-xl hover:bg-red-600"
       type="submit"
       onClick={(e) => handleSubmit && handleSubmit(e)}
     >
--- src/containers/LoginRegisterContainer/LoginRegisterContainer.jsx
+++ src/containers/LoginRegisterContainer/LoginRegisterContainer.jsx
@@ -4,14 +4,10 @@ const LoginRegisterContainer = ({ FormContainer }) => {
       <img
         src="./images/login-bg.jpg"
         alt=""
-        className="absolute object-cover w-screen h-screen -z-10"
+        className="hidden md:block md:object-cover md:w-screen md:h-screen md:absolute -z-10"
       />
-      <section className="w-[80%] lg:w-[800px] md:h-[800px] rounded-md border py-5  flex gap-5 items-center justify-center flex-col backdrop-blur-sm bg-black/60">
-        <img
-          src="/images/watchflix.png"
-          alt="Logo"
-          className="w-[375px] sm:w-[475px] md:w-auto "
-        />
+      <section className=" lg:w-[800px] md:h-[800px] rounded-md md:border py-5 flex gap-5 items-center justify-center flex-col backdrop-blur-sm bg-black/60">
+        <img src="/images/watchflix.png" alt="Logo" className="" />
         <p className="mb-10 -mt-5 text-xs text-center text-white">
           Where the movies are
         </p>
