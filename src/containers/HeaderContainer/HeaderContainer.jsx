aef36fc11f93c9e77290e20f729ac162753e7f32
damyvegaleiva <damyvegaleiva@gmail.com>
2025-05-30 03:59:25 -0500

    Fixed Header background-image route.

--- src/containers/HeaderContainer/HeaderContainer.jsx
+++ src/containers/HeaderContainer/HeaderContainer.jsx
@@ -3,7 +3,7 @@ import HeaderFormContainer from "../../components/Header/HeaderFormContainer";
 
 const HeaderContainer = () => {
   return (
-    <header className="mb-16 md:mb-28 bg-[url('./images/login-bg.jpg')] bg-cover relative z-50 before:content-[''] before:w-full before:h-full before:-z-[1] before:absolute before:bg-black/75 pb-5">
+    <header className="mb-16 md:mb-28 bg-[url('../images/login-bg.jpg')] bg-cover relative z-50 before:content-[''] before:w-full before:h-full before:-z-[1] before:absolute before:bg-black/75 pb-5">
       <HeaderImage />
       <HeaderFormContainer />
     </header>
